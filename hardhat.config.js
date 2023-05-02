3423cac8444c818b001e48f6008157893f39eddc
Quentin <thealmightyquinn@hotmail.com>
2023-05-02 10:59:10 +0200

    update registry.sol no verify

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,10 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-toolbox");
+
 module.exports = {
   solidity: "0.8.19",
 };
+
+task ('hasher','Compile Poseidon hasher', () => {
+  require('./scripts/compilePoseidon.js')
+})
